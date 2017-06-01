662f3b723f3ec26e45bf2b6a4fbd649f6b8d762b
root <root@ALG-rundesk.lab.nantes>
2017-06-01 10:14:50 +0200

    rectification chemin CSS

--- application/views/ajouter_invite.php
+++ application/views/ajouter_invite.php
@@ -9,7 +9,7 @@
         <meta name="description" content="Connexion à mon application">
         <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
         <!-- ci-dessous notre fichier CSS -->
-        <link rel="stylesheet" type="text/css" href="http://<?php echo base_url(); ?>portailinvite/assets/css/app.css" />
+        <link rel="stylesheet" type="text/css" href="http://localhost/portailinvite/assets/css/app.css" />
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:400,700,300" />
         <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
